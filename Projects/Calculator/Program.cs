1a4704a1a5942763b029b32261faab783d60fc92
s.sulaberidze <s.sulaberidze.doit@itstep.ge>
2024-11-29 16:34:32 +0400

    change calculator

--- Projects/Calculator/Program.cs
+++ Projects/Calculator/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Transactions;
-
-public class Calculator
+﻿public class Calculator
 {
     public double Add(double a, double b) => a + b;
     public double Subtract(double a, double b) => a - b;
@@ -16,64 +14,70 @@ class Program
     static void Main(string[] args)
     {
         Calculator calc = new Calculator();
+        bool continueCalculations = true;
+        double previousResult = 0;
+        bool isFirstCalculation = true;
 
-        CalculatorDelegate addDelegate = new CalculatorDelegate(calc.Add);
-        CalculatorDelegate subtractDelegate = new CalculatorDelegate(calc.Subtract);
-        CalculatorDelegate multiplyDelegate = new CalculatorDelegate(calc.Multiply);
-        CalculatorDelegate divideDelegate = new CalculatorDelegate(calc.Divide);
-        CalculatorDelegate modulusDelegate = new CalculatorDelegate(calc.Modulus);
-
-        CalculatorDelegate allOperations = addDelegate;
-        allOperations += subtractDelegate;
-        allOperations += multiplyDelegate;
-        allOperations += divideDelegate;
-        allOperations += modulusDelegate;
-
-        Console.WriteLine("Enter two numbers");
-
-        double a;
-        bool success = double.TryParse(Console.ReadLine(), out a);
-
-        double b;
-        success = double.TryParse(Console.ReadLine(), out b);
-        if (!success)
+        while (continueCalculations)
         {
-            Console.WriteLine("Please enter a valid number.");
-            return;
-        }
+            double a, b;
 
-        Console.WriteLine("Now enter operation symbol");
+            if (isFirstCalculation)
+            {
+                Console.WriteLine("Enter the first number:");
+                if (!double.TryParse(Console.ReadLine(), out a))
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                    continue;
+                }
+                isFirstCalculation = false;
+            }
+            else
+            {
+                a = previousResult;
+                Console.WriteLine($"Using previous result: {a}");
+            }
 
-        var incomeLine = Console.ReadLine();
+            Console.WriteLine("Enter the second number:");
+            if (!double.TryParse(Console.ReadLine(), out b))
+            {
+                Console.WriteLine("Please enter a valid number.");
+                continue;
+            }
 
-        CalculatorDelegate operation = new CalculatorDelegate(calc.Add);
+            Console.WriteLine("Now enter operation symbol (+, -, *, /, %):");
+            string operationSymbol = Console.ReadLine();
 
-        if (incomeLine == "+")
-        {
-            operation = new CalculatorDelegate(calc.Add);
-        }
-        else if (incomeLine == "-")
-        {
-            operation = new CalculatorDelegate(calc.Subtract);
-        }
-        else if (incomeLine == "*")
-        {
-            operation = new CalculatorDelegate(calc.Multiply);
-        }
-        else if (incomeLine == "/")
-        {
-            operation = new CalculatorDelegate(calc.Divide);
-        }
-        else if (incomeLine == "%")
-        {
-            operation = new CalculatorDelegate(calc.Modulus);
-        }
-        else
-        {
-            Console.WriteLine("Entered operation is not valid");
-            return;
-        }
+            CalculatorDelegate operation = operationSymbol switch
+            {
+                "+" => calc.Add,
+                "-" => calc.Subtract,
+                "*" => calc.Multiply,
+                "/" => calc.Divide,
+                "%" => calc.Modulus,
+                _ => null
+            };
 
-        Console.WriteLine(operation(a, b));
+            if (operation == null)
+            {
+                Console.WriteLine("Entered operation is not valid");
+                continue;
+            }
+
+            try
+            {
+                previousResult = operation(a, b);
+                Console.WriteLine($"Result: {previousResult}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+                continue;
+            }
+
+            Console.WriteLine("Do you want to perform another calculation with this result? (yes/no):");
+            string continueResponse = Console.ReadLine().ToLower();
+            continueCalculations = continueResponse == "yes";
+        }
     }
 }
